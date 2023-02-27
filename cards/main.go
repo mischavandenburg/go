925cbe36b55a8d3be574a55a155c0ec100ef448a
Mischa van den Burg <mischa@mac-beast>
2023-02-27 20:37:13 +0100

    typing a function

--- cards/main.go
+++ cards/main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Spades"
-	card := "Five of Diamonds"
+	card := newCard()
 	fmt.Println(card)
+}
+
+// the word string tells the compiler that the function will always return a string
+func newCard() string {
+	return "Five of Diamonds"
 }
