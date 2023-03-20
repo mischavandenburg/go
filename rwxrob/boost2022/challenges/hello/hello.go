03ed940dff8d16c9d52d498207aa843b75589cd3
Mischa van den Burg <mischa@mac-beast>
2023-03-20 07:12:06 +0100

    hello challenge

---
+++ rwxrob/boost2022/challenges/hello/hello.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+
+	// more ways to print
+	// This logs and reirects to stderr
+	log.Println("Hello World, using log")
+}
