24f1eccb29f68906441a49e09cc1ec189c958acc
Mischa van den Burg <mischavandenburg@gmail.com>
2023-03-26 14:02:03 +0200

    added french greeting

--- rwxrob/boost2022/challenges/greet/greet.go
+++ rwxrob/boost2022/challenges/greet/greet.go
@@ -1,7 +1,13 @@
 package greet
 
+// to run use go install ./cmd/salut; salut or cmd/greet; greet
+
 import "fmt"
 
 func Hi(name string) {
 	fmt.Printf("Hi, %v, nice to meet you!\n", name)
+}
+
+func Salut(name string) {
+	fmt.Printf("Salut, %v, ravi de faire votre connaissance!\n", name)
 }
